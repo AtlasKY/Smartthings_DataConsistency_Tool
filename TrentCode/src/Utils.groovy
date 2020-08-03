68fd4907bc38b9611d7b0df9d8b143b725dabbd5
AtlasKY <54989889+AtlasKY@users.noreply.github.com>
2020-08-03 11:20:28 +0300

    Initial commit

---
+++ TrentCode/src/Utils.groovy
@@ -0,0 +1,43 @@
+/*
+ * SmartThingsAnalysisTools Copyright 2016 Regents of the University of Michigan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package edu.umich.smartthings.overprivilege;
+
+public class Utils
+{
+	public static String[][] allUniqueCombinations(LinkedHashMap<String, Vector<String>> dataStructure)
+	{
+		int n = dataStructure.keySet().size().intValue();
+		int solutions = 1;
+
+		for(Vector<String> vector : dataStructure.values()) {
+			solutions = solutions * vector.size().intValue();
+		}
+
+		String[][] allCombinations = new String[solutions + 1][];
+		allCombinations[0] = dataStructure.keySet().toArray(new String[n]);
+
+		for(int i = 0; i < solutions; i++) {
+			Vector<String> combination = new Vector<String>(n);
+			int j = 1;
+			for(Vector<String> vec : dataStructure.values()) {
+				// combination.add(vec.get(((int) i/j) % vec.size().intValue()));
+				// the above line fails to compile. It has been broken out into individual steps below, which compiles successfully.
+				int vecSize = vec.size().intValue();
+				int sk =  ((int)i / j);
+				int amalgam = sk % vecSize;
+				String s = vec.get(amalgam);
+				combination.add(s);
+				j = j * vec.size().intValue();
+			}
+			allCombinations[i + 1] = combination.toArray(new String[n]);
+		}
+
+		return allCombinations;
+	}
+}
